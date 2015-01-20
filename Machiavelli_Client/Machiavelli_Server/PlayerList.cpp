f79714f97ebfea8317fcbac95bbcff4afb1f9ad3
Trevorr Marshall <fox4life76@gmail.com>
2015-01-20 20:33:53 +0100

    Changes by toopak chopra

--- Machiavelli_Client/Machiavelli_Server/PlayerList.cpp
+++ Machiavelli_Client/Machiavelli_Server/PlayerList.cpp
@@ -3,70 +3,106 @@
 
 PlayerList::PlayerList()
 {
-
 }
 
+PlayerList::~PlayerList()
+{
+}
 
-
-shared_ptr<Player> PlayerList::GetPlayerAt(int position){
-	if (position < 0 || position >= Size()) return nullptr;
+shared_ptr<Player> PlayerList::GetPlayerAt(int position)
+{
+	if (position < 0 || position >= Size()) 
+		return nullptr;
 	return m_PlayerList.at(position);
 }
 
-shared_ptr<Player> PlayerList::GetPlayerByRole(eCharacterCard card){
+shared_ptr<Player> PlayerList::GetPlayerByRole(eCharacterCard card)
+{
 	shared_ptr<Player> player{ nullptr };
-	for (int i = 0; i < m_PlayerList.size() && player == nullptr; i++){
-		if (m_PlayerList.at(i)->HasCharacterCard(card)){
+	for (auto& i : m_PlayerList) 
+	{
+		if (i->HasCharacterCard(card))
+			return i;
+	}
+	return nullptr;
+	/*for (int i = 0; i < m_PlayerList.size() && player == nullptr; i++)
+	{
+		if (m_PlayerList.at(i)->HasCharacterCard(card))
+		{
 			player = m_PlayerList.at(i);
 		}
 	}
-	return player;
+	return player;*/
 }
-shared_ptr<Player> PlayerList::GetPlayerByName(string name){
+shared_ptr<Player> PlayerList::GetPlayerByName(string name)
+{
 	shared_ptr<Player> player{ nullptr };
-	for (int i = 0; i < m_PlayerList.size() && player == nullptr; i++){
-		if (m_PlayerList.at(i)->GetName() == name){
+	for (auto& i : m_PlayerList)
+	{
+		if (i->GetName() == name)
+			return i;
+	}
+	return nullptr;
+	/*shared_ptr<Player> player{ nullptr };
+	for (int i = 0; i < m_PlayerList.size() && player == nullptr; i++)
+	{
+		if (m_PlayerList.at(i)->GetName() == name)
+		{
 			player = m_PlayerList.at(i);
 		}
 	}
-	return player;
+	return player;*/
 }
-void PlayerList::InsertPlayer(shared_ptr<Player> player){
-	m_PlayerList.push_back(player);
-}
-void PlayerList::RemovePlayer(shared_ptr<Player> player){
 
+void PlayerList::RemovePlayer(shared_ptr<Player> player)
+{
 	auto position = std::find(m_PlayerList.begin(), m_PlayerList.end(), player);
-	if (position != m_PlayerList.end()){
+	if (position != m_PlayerList.end())
+	{
 		m_PlayerList.erase(position);
 	}
 
 }
 
-void PlayerList::SendAll(string message){
-	for (int i = 0; i < m_PlayerList.size(); i++){
-		m_PlayerList.at(i)->Send(message);
+void PlayerList::SendAll(string message)
+{
+	for (auto& i : m_PlayerList)
+	{
+		i->Send(message);
 	}
+
+	/*for (int i = 0; i < m_PlayerList.size(); i++)
+	{
+		m_PlayerList.at(i)->Send(message);
+	}*/
 }
 
 
-void PlayerList::SendAllBut(shared_ptr<Player> player, string message){
-	for (int i = 0; i < m_PlayerList.size(); i++){
-		shared_ptr<Player> playerCompare = m_PlayerList.at(i);
-		if (playerCompare.get() != player.get()){
-			playerCompare->Send(message);
+void PlayerList::SendAllBut(shared_ptr<Player> player, string message)
+{
+	for (auto& i : m_PlayerList)
+	{
+		if (i.get() != player.get())
+		{
+			i->Send(message);
 		}
 	}
+	/*for (int i = 0; i < m_PlayerList.size(); i++)
+	{
+		shared_ptr<Player> playerCompare = m_PlayerList.at(i);
+		if (playerCompare.get() != player.get())
+		{
+			playerCompare->Send(message);
+		}
+	}*/
 }
 
-int PlayerList::Size(){
-	return m_PlayerList.size();
-}
-
-void PlayerList::MoveToFront(shared_ptr<Player> player) {
+void PlayerList::MoveToFront(shared_ptr<Player> player)
+{
 	vector<shared_ptr<Player>> newPlayerList{player};
 
-	for (auto &plr : m_PlayerList) {
+	for (auto &plr : m_PlayerList) 
+	{
 		if (plr != player) {
 			newPlayerList.push_back(plr);
 		}
@@ -75,6 +111,3 @@ void PlayerList::MoveToFront(shared_ptr<Player> player) {
 	m_PlayerList = newPlayerList;
 }
 
-PlayerList::~PlayerList()
-{
-}
--- Machiavelli_Client/Machiavelli_Server/PlayerList.h
+++ Machiavelli_Client/Machiavelli_Server/PlayerList.h
@@ -10,21 +10,25 @@ using std::shared_ptr;
 
 class PlayerList
 {
-private:
-	vector<shared_ptr<Player>> m_PlayerList;
-	vector<shared_ptr<int>> m_Temp;
 public:
 	PlayerList();
+	virtual ~PlayerList();
 
 	shared_ptr<Player> GetPlayerAt(int position);
 	shared_ptr<Player> GetPlayerByRole(eCharacterCard card);
 	shared_ptr<Player> GetPlayerByName(string name);
-	void InsertPlayer(shared_ptr<Player> player);
+
+	void InsertPlayer(shared_ptr<Player> player) { m_PlayerList.push_back(player); }
 	void RemovePlayer(shared_ptr<Player> player);
 	void SendAll(string message);
 	void SendAllBut(shared_ptr<Player> player, string message);
 	void MoveToFront(shared_ptr<Player> player);
-	int Size();
 
-	virtual ~PlayerList();
+	int Size(){	return m_PlayerList.size(); }
+
+
+
+private:
+	vector<shared_ptr<Player>> m_PlayerList;
+	vector<shared_ptr<int>> m_Temp;
 };
