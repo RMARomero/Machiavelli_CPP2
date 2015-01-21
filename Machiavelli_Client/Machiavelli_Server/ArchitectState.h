c030a1f7b4e6a9383627110275b522a8941c3425
RMARomero <romero.ricardo09@outlook.com>
2015-01-21 01:51:07 +0100

    initial changes

--- Machiavelli_Client/Machiavelli_Server/ArchitectState.h
+++ Machiavelli_Client/Machiavelli_Server/ArchitectState.h
@@ -1,14 +1,17 @@
 #pragma once
 #include "IRoundState.h"
-class ArchitectState :
-	public IRoundState
+
+class ArchitectState : public IRoundState
 {
 public:
 	ArchitectState();
+	~ArchitectState();
+
 	void Handle(GameRunningState& context, GameManager& gm);
 	void PickDistrictCard(shared_ptr<CardPile<DistrictCard>> cp, int amount);
 	void BuildSomething();
+
 	eCharacterCard currentRole();
-	~ArchitectState();
+	
 };
 
